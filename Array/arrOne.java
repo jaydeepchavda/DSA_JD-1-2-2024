e1419812cd88bc8011463e21b3bf144f86fec2e6
jaydipchavda2216@gmail.com <jaydipchavda2216@gmail.com>
2024-02-10 23:22:49 +0530

    print array in java commit

--- Array/arrOne.java
+++ Array/arrOne.java
@@ -1,1 +1,18 @@
-first program as java developer
+// first program as java developer
+
+public class arrOne{
+    public static void main(String[] args){
+        int[] numbers = {1, 2, 3, 4, 5};
+
+        // Using an enhanced for loop to print each element
+        System.out.print("Array elements: ");
+        // for (int num : numbers) {
+        //     System.out.print(num + " ");
+        // }
+        for(int i = 0; i<numbers.length;i++){
+            System.out.print(numbers[i]+ " ");
+        }
+        System.out.println();
+        }
+        
+    }
